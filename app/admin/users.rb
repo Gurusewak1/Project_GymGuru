cdeef2eda7de0b3a7f8bb2dab528e573a8a1552e
Gurusewak1 <gurusewaksinghbains@gmail.com>
2024-07-09 17:43:28 -0500

    category

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,25 @@
 ActiveAdmin.register User do
+  permit_params :email, :password, :password_confirmation, :admin
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :username
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :username]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  form do |f|
+    f.inputs "User Details" do
+      f.input :name
+      f.input :email
+      f.input :password
+      f.input :password_confirmation
+     
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :name
+    actions
+  end
+
+  filter :email
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 # app/models/user.rb
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    # Devise modules
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
   
+    # Ransackable attributes for searching
+    def self.ransackable_attributes(auth_object = nil)
+      ["created_at", "email", "encrypted_password", "id", "id_value", "name", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at", "username"]
+    end
   
     # Validations
     validates :email, presence: true, uniqueness: true
