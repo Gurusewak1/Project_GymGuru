13cb1bf3558422ba9b2eec3f0521f909a189fc3e
Gurusewak1 <gurusewaksinghbains@gmail.com>
2024-07-30 15:43:23 -0500

    denormalise schema

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,14 @@ class Order < ApplicationRecord
   belongs_to :province
 
 
+  def self.ransackable_associations(auth_object = nil)
+    ["order_items", "products", "province", "user"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "created_at", "gst", "hst", "id", "id_value", "payment_id", "province", "province_id", "pst", "qst", "status", "stripe_payment_intent_id", "subtotal", "total", "total_amount", "updated_at", "user_id"]
+  end
+
   def calculate_total
     # Calculate total based on other attributes
     self.total = subtotal + gst + hst + pst + qst
